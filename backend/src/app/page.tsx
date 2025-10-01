53ad49f4a63eb679a7ab479eacb0afa051700f24
Mateus Fortes <96443389+mateuskfortes@users.noreply.github.com>
2025-09-30 21:25:16 -0300

    feat(home): add home page

--- backend/src/app/page.tsx
+++ backend/src/app/page.tsx
@@ -1,103 +1,68 @@
 import Image from "next/image";
+import '@/styles/home.css'
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <>
+      <div className="hero">
+        <section className="white-board">
+          <h1><span className="highlight">Melodia Educacional:</span><br />A linguagem da música ao seu alcance</h1>
+        </section>
+        <p className="subtitle">
+          Website com intuito de ensinar de teoria musical<br />
+          <strong>com prática e acessibilidade.</strong>
+        </p>
+        <p className="note">
+          Criado por estudantes do Instituto Federal Catarinense Campus Camboriú<br />
+          <small>*O Melodia Educacional é um projeto independente, criado com o objetivo de incentivar o ensino de
+            teoria musical. Não somos especialistas. Saiba mais.</small>
+        </p>
+        <div className="cta-buttons">
+          <a href="#" className="btn red">Iniciar</a>
+          <a href="register" className="btn outline">Cadastrar-se</a>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <div className="hero-image">
           <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+            objectFit="contain"
+            src="/images/hero.png"
+            alt="Ilustração músico"
+            fill
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        </div>
+      </div>
+
+      <section className="features">
+        <h2>Características do Melodia Musical</h2>
+        <a href="/lesson" className="view-all">Ver Todas as Aulas</a>
+
+        <div className="feature-box">
+          <div className="number">01</div>
+          <div className="content">
+            <h3>Aulas de Música:</h3>
+            <p>Clique aqui para acessar as aulas completas, onde você aprende teoria, leitura de notas e prática musical.</p>
+          </div>
+          <a href="/lesson"><span className="arrow">↗</span></a>
+        </div>
+
+        <div className="feature-box">
+          <div className="number">02</div>
+          <div className="content">
+            <h3>Exercícios Interativos:</h3>
+            <p>Pratique com atividades interativas como quizzes.</p>
+          </div>
+          <a href="/exercises"><span className="arrow">↗</span></a>
+        </div>
+
+        <div className="feature-box">
+          <div className="number">03</div>
+          <div className="content">
+            <h3>Criação Musical:</h3>
+            <p>Use nossas ferramentas para compor e criar suas próprias músicas de forma simples e intuitiva.</p>
+          </div>
+          <a href="/sheet-music"><span className="arrow">↗</span></a>
+        </div>
+
+      </section>
+    </>
   );
 }

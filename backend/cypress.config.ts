12b1c79a334dfa3746e0b694bdd5826a75d1c01b
Mateus Fortes <96443389+mateuskfortes@users.noreply.github.com>
2025-10-07 18:31:27 -0300

    update(cypress): remove env variables

--- backend/cypress.config.ts
+++ backend/cypress.config.ts
@@ -2,12 +2,11 @@ import { defineConfig } from "cypress";
 import * as dotenv from "dotenv";
 import registerPlugins from "./cypress/plugins";
 
-// Carrega .env para que process.env.CYPRESS_BASE_URL seja preenchido ao iniciar o Cypress
 dotenv.config({ path: ".env" });
 
 export default defineConfig({
   e2e: {
-    baseUrl: process.env.SITE_URL || "http://localhost:3000",
+    baseUrl: "http://localhost:3000",
     specPattern: "cypress/**/*.spec.ts",
     supportFile: "cypress/support/index.ts",
     setupNodeEvents(on, config) {

97e9217a8ec94c39d8b0445d969e55b66d1f4ae2
Mateus Fortes <96443389+mateuskfortes@users.noreply.github.com>
2025-10-17 09:39:48 -0300

    update auth

--- backend/src/components/Header.tsx
+++ backend/src/components/Header.tsx
@@ -6,36 +6,40 @@ import { signOut, useSession } from 'next-auth/react';
 import { useEffect } from 'react';
 
 export default function Header() {
-  const { data: session } = useSession();
+  const { data: session, status } = useSession();
+
   useEffect(() => {
-    if (session?.error) signOut({ callbackUrl: '/login' })
-  })
+    if (session?.error) {
+      signOut({ callbackUrl: '/login' });
+    }
+  }, [session]);
+
   return (
-    <>
-      <header className="header">
-        <Link href="/" className="logo">Melodia Educacional</Link>
-        <nav className="nav">
-          <ul>
-            <li>
-              {!session && (
-                <Link href="/register" className="nav-link active">Cadastro</Link>
-              )}
-            </li>
-            <li><a href="#">Tutorial</a></li>
-            <li><a href="#">Quem somos</a></li>
-            <li>
-              {session ? (
-                <button
-                  onClick={() => signOut({ callbackUrl: '/' })}
-                  className="login-button"
-                >Sair</button>
-              ) : (
-                <Link href="/login" className="login-button">Login</Link>
-              )}
-            </li>
-          </ul>
-        </nav>
-      </header>
-    </>
+    <header className="header">
+      <Link href="/" className="logo">Melodia Educacional</Link>
+      <nav className="nav">
+        <ul>
+          <li>
+            {status !== 'loading' && !session && (
+              <Link href="/register" className="nav-link active">Cadastro</Link>
+            )}
+          </li>
+          <li><a href="#">Tutorial</a></li>
+          <li><a href="#">Quem somos</a></li>
+          <li>
+            {status === 'loading' ? null : session ? (
+              <button
+                onClick={() => signOut({ callbackUrl: '/' })}
+                className="login-button"
+              >
+                Sair
+              </button>
+            ) : (
+              <Link href="/login" className="login-button">Login</Link>
+            )}
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 }
